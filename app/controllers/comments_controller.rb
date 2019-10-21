562ca1617ad6461b7a8b0f91a4de114b5512ad5b
chungcha <charlieschung@gmail.com>
2019-10-21 15:15:11 -0400

    added some crud for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,11 @@
 class CommentsController < ApplicationController
 
     def new
+        @comment = Comment.new
     end
 
     def create
+        @comment = Comment.new(comment_params)
     end
 
     def edit
@@ -14,5 +16,10 @@ class CommentsController < ApplicationController
 
     def destroy
     end    
+
+    private
+    def comment_params
+        params.require(:comment).permit(:content, :audience_id,:item_id)
+    end
 
 end
--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,11 +1,18 @@
 class FollowsController < ApplicationController
 
     def new
-        
+        @follow = Follow.new(follow_params)
     end
 
     def create
+        @follow = Follow.create(follow_params)
 
+    end
+
+    private
+    
+    def follow_params
+        params.require(:follow).permit(:audience_id, :list_id)
     end
 
 end
