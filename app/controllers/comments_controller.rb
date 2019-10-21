ebec9bf47d59d1d451b2d6cd1902355a1aadea43
chungcha <charlieschung@gmail.com>
2019-10-21 15:02:23 -0400

    made crud functions for comment/follow

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end   
+
+    def update
+    end
+
+    def destroy
+    end    
+
 end
