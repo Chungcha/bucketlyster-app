ed15a4310130ed5fcc5c7fe19029855032e189ef
chungcha <charlieschung@gmail.com>
2019-10-22 10:06:46 -0400

    got the comments form working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :follows, only: [:create, :destroy]
-  resources :comments
-  resources :items
+
+  resources :items do 
+    resources :comments
+  end
   resources :lists
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
