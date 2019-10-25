9fc3e52876634a80a18998b0022777808beeacdb
chungcha <charlieschung@gmail.com>
2019-10-25 11:07:59 -0400

    some changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
     def create
         @item = Item.find(params[:item_id])
         @comment = @item.comments.create(comment_params)
-   
+        
         redirect_to item_path(@item)
     end 
  
